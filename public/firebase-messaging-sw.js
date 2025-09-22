2cbc872feb1c281513b512fc47ace6d79f9a5214
Gabriel Maia <gabrielmaiaads1@gmail.com>
2025-09-22 11:15:08 +0000

    demora em media quanto tempo? pq ja passou 1 minuto e continua em "ativa

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,35 +1,31 @@
-/* eslint-disable no-undef */
-// Importa compat para SW
-importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-// Cole aqui a configuração do seu projeto Firebase.
-// Esta configuração é segura para ser pública.
-const firebaseConfig = {
-  "projectId": "adcraft-blueprint",
-  "appId": "1:1071558588370:web:b225fd25847301b03bfa0b",
-  "apiKey": "AIzaSyCKtS_ZSgzfcfo-l571JhsaOffkphcXFx0",
-  "authDomain": "adcraft-blueprint.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "1071558588370"
-};
+// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+// O sender ID precisa ser o mesmo do seu projeto
+firebase.initializeApp({
+    apiKey: "AIzaSyCKtS_ZSgzfcfo-l571JhsaOffkphcXFx0",
+    authDomain: "adcraft-blueprint.firebaseapp.com",
+    projectId: "adcraft-blueprint",
+    storageBucket: "adcraft-blueprint.appspot.com",
+    messagingSenderId: "1071558588370",
+    appId: "1:1071558588370:web:b225fd25847301b03bfa0b"
+});
 
-firebase.initializeApp(firebaseConfig);
 
-// Necessário para receber push em background
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
 const messaging = firebase.messaging();
 
-// (Opcional) personalizar notificação recebida em background
-messaging.onBackgroundMessage((payload) => {
-  console.log(
-    '[firebase-messaging-sw.js] Received background message ',
-    payload
-  );
-  const notificationTitle = payload.notification?.title || 'Novo Evento';
+messaging.onBackgroundMessage(function(payload) {
+  console.log('Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
   const notificationOptions = {
-    body: payload.notification?.body || 'Você tem um alerta do site.',
-    icon: '/icon-192.png', // Opcional: adicione um ícone na pasta public
+    body: payload.notification.body,
+    icon: '/logo-escura.png'
   };
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
 });
